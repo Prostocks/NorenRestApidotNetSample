47b2d58dbe0fd8064f5367e417019a13cca076b5
Nikhesh <nikhesh.p@kambala.co.in>
2024-01-10 11:53:14 +0530

    endpoints updated

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -15,8 +15,8 @@ namespace NorenRestSample
     {
         #region dev  credentials
 
-        public const string endPoint = "http://api.noren.com/NorenWClient/";
-        public const string wsendpoint = "wss://api.noren.com/NorenWSWeb/";
+        public const string endPoint = "https://starapiuat.prostocks.com/NorenWClientTP/";
+        public const string wsendpoint = "wss://starapiuat.prostocks.com/NorenWS/";
         public const string uid = "";
         public const string actid = "";
         public const string pwd = "";
